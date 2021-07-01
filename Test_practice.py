21132b0a919b0155839004bd7818ef1d6a53fa35
Dini Hassan <dinih51@gmail.com>
2021-07-01 10:29:32 +0100

    First

---
+++ Test_practice.py
@@ -0,0 +1,13 @@
+ # Declare a function called user name that takes one arg as a str and return the name
+name = "dini"
+def user_name(name):
+    return name
+print(user_name(name))
+
+# Declare a list with num 1-5 and iterate through the list and display list
+
+list = [1.,2,3,4,5]
+for num in list:
+    print(num)
+
+

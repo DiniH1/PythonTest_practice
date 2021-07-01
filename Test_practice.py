a26a5175b2541c5166b1ba0fe199886336e3dce0
Dini Hassan <dinih51@gmail.com>
2021-07-01 10:35:39 +0100

    Questions so far

--- Test_practice.py
+++ Test_practice.py
@@ -1,13 +1,20 @@
  # Declare a function called user name that takes one arg as a str and return the name
-name = "dini"
-def user_name(name):
-    return name
-print(user_name(name))
+# name = "dini"
+# def user_name(name):
+#     return name
+# print(user_name(name))
+#
+# # Declare a list with num 1-5 and iterate through the list and display list
+#
+# list = [1.,2,3,4,5]
+# for num in list:
+#     print(num)
+#
+# # What is the difference between a list and a tuple
+# # A list is mutable
+# # A tuple is immutable
 
-# Declare a list with num 1-5 and iterate through the list and display list
-
-list = [1.,2,3,4,5]
-for num in list:
-    print(num)
-
+# Can we add an element to a list yes
+# Can we add an element to a tuple yes
+# Can the element of a tuple be different types yes
 
